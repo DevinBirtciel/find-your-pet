59e51cfd7979ece0c98c11360ba04275c48948fa
Devin B <devinb@Devins-MacBook-Pro.local>
2025-05-08 16:38:54 -0700

    cdk init

---
+++ test/find-your-pet.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FindYourPet from '../lib/find-your-pet-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/find-your-pet-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FindYourPet.FindYourPetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
